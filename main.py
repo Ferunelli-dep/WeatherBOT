abd25dbef5b6ff08c34f1107ac7221e83f8eb0b4
Nikita Podpruzhnikov <ferunelli222@gmail.com>
2020-04-11 23:29:15 +0300

    BOT commented main

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ from vk_server import vk_server
 import pymysql
 
 
-current = Weather("Москва", region="RU", API=data.API)
-
-db = DB(data.database['host'], data.database['username'],
-        data.database['password'], data.database['database'],
-        data.database['port'])
+# current = Weather("Москва", region="RU", API=data.API)
+#
+# db = DB(data.database['host'], data.database['username'],
+#         data.database['password'], data.database['database'],
+#         data.database['port'])
 
 
 vk = vk_server(data.vk_token, data.bot_id)
 vk.start()
 
 
-if db.test():
-    print(db.test())
-else:
-    print("NOpe")
+# if db.test():
+#     print(db.test())
+# else:
+#     print("NOpe")
 
 
